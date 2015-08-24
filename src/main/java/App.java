f9141f0094f87fb3bd0b873cf038eec04e98a2d3
chitra atmakuru & daryl seaver <student@epicodus.com>
2015-08-24 15:53:06 -0700

    Add completed app

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -18,7 +18,7 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-    get("categories/new", (request, response) -> {
+    get("category/new", (request, response) -> {
       HashMap<String, Object> model = new HashMap<String, Object>();
       model.put("template", "templates/category-form.vtl");
       return new ModelAndView(model, layout);
@@ -34,12 +34,6 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-    // get("/categories", (request, response) -> {
-    //   HashMap<String, Object> model = new HashMap<String, Object>();
-    //   model.put("categories", Category.all());
-    //   model.put("template", "templates/category.vtl");
-    //   return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine());
     get("categories/:id/tasks", (request, response) -> {
       HashMap<String, Object> model = new HashMap<String, Object>();
       Category category = Category.find(Integer.parseInt(request.params(":id")));
@@ -63,37 +57,10 @@ public class App {
     }, new VelocityTemplateEngine());
 
 
-
-    // get("/tasks", (request, response) -> {
-    //   HashMap<String, Object> model = new HashMap<String, Object>();
-    //   model.put("tasks", Task.all());
-    //   model.put("template", "templates/tasks.vtl");
-    //   return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine());
-    //
-    //
-    // get("tasks/new", (request, response) -> {
-    //   HashMap<String, Object> model = new HashMap<String, Object>();
-    //   model.put("template", "templates/task-form.vtl");
-    //   return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine());
-
     post("/tasks", (request, response) -> {
       HashMap<String, Object> model = new HashMap<String, Object>();
 
-      // Category category = Category.find(Integer.parseInt(request.queryParams("categoryId")));
-      // ArrayList<Task> tasks = category.getTasks();
-      //
-      // if (tasks == null) {
-      //   tasks = new ArrayList<Task>();
-      //   request.session().attribute("tasks", tasks);
-      // }
-      //
-      // String description = request.queryParams("description");
-      // Task newTask = new Task(description);
-      //
-      // tasks.add(newTask);
-      Category category = Category.find(Integer.parseInt(request.queryParams("categoryId")));
+      Category category = Category.find(Integer.parseInt(request.queryParams("catId")));
       int catId = category.getId();
       String description = request.queryParams("description");
       Task newTask = new Task(description, catId);
